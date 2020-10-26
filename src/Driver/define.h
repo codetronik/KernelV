b1000171e6cf182a2aa22851578cc83705ea21dd
codetronik <codetronik@gmail.com>
2020-10-26 23:56:17 +0900

    KdPrintEx로 변경

--- src/Driver/define.h
+++ src/Driver/define.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define kprintf(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, __VA_ARGS__) // DbgPrint()를 사용한 경우 로딩이 되지 않았던 문제 발생
+#define kprintf(...) KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, __VA_ARGS__)) // DbgPrint()를 사용한 경우 로딩이 되지 않았던 문제 발생
 #define POOL_TAG 'coTr'
 
